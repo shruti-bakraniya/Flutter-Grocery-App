a624075daca3cd39d4d1a7203095ef3293fbb87e
shruti-bakraniya <shruti.bakraniya@gmail.com>
2025-02-24 02:35:52 +0530

    fix: errors

---
+++ lib/features/home/bloc/home_bloc.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_grocery_app/data/grocery_data.dart';
+import 'package:flutter_grocery_app/features/home/models/home_product_data_model.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc() : super(HomeInitial()) {
+    on<HomeInitialEvent>(homeInitialEvent);
+
+    on<HomeProductWishlistButtonClickedEvent>(
+        homeProductWishlistButtonClickedEvent);
+
+    on<HomeProductCartButtonClickedEvent>(homeProductCartButtonClickedEvent);
+
+    on<HomeWishlistButtonNavigateEvent>(homeWishlistButtonNavigateEvent);
+
+    on<HomeCartButtonNavigateEvent>(homeCartButtonNavigateEvent);
+  }
+
+  FutureOr<void> homeInitialEvent(
+      HomeInitialEvent event, Emitter<HomeState> emit) async {
+    emit(HomeLoadingState());
+    await Future.delayed(const Duration(seconds: 3));
+    try {
+      emit(HomeLoadedSuccessState(
+          products: GroceryData.groceryProducts.map((e) => ProductDataModel(
+            id: e['id'],
+            name: e['name'],
+            description: e['description'],
+            imageUrl: e['imageUrl'],
+            price: e['price'],
+          )).toList())
+      );
+    } catch (e) {
+      emit(HomeErrorState());
+    }
+  }
+
+  FutureOr<void> homeProductWishlistButtonClickedEvent(
+      HomeProductWishlistButtonClickedEvent event, Emitter<HomeState> emit) {
+    print("KDEBUG:: Wishlist Button Clicked");
+    // wishlistItems.add(event.clickedProduct);
+    emit(HomeProductItemWishlistedActionState());
+  }
+
+  FutureOr<void> homeProductCartButtonClickedEvent(
+      HomeProductCartButtonClickedEvent event, Emitter<HomeState> emit) {
+    print("KDEBUG:: Cart Button Clicked");
+    // cartItems.add(event.clickedProduct);
+    emit(HomeProductItemCartedActionState());
+  }
+
+  FutureOr<void> homeWishlistButtonNavigateEvent(
+      HomeWishlistButtonNavigateEvent event, Emitter<HomeState> emit) {
+    print("KDEBUG:: Wishlist Button Navigate");
+    emit(HomeNavigateToWishlistPageActionState());
+  }
+
+  FutureOr<void> homeCartButtonNavigateEvent(
+      HomeCartButtonNavigateEvent event, Emitter<HomeState> emit) {
+    print("KDEBUG:: Cart Button Navigate");
+    emit(HomeNavigateToCartPageActionState());
+  }
+}
